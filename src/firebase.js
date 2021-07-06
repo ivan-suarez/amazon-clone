1c446e6d093e28e85fb4cba8ee33b16a5deec9ab
ivan-suarez <heyitsivan@protonmail.com>
2021-07-06 13:37:16 +0200

    Added firebase support

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCfg2glz_L2JLAFG8bERWJIPS8ymn7O-_k",
+    authDomain: "clone-fabef.firebaseapp.com",
+    projectId: "clone-fabef",
+    storageBucket: "clone-fabef.appspot.com",
+    messagingSenderId: "1080223649042",
+    appId: "1:1080223649042:web:06e1580679ef714c3fb7cb"
+  };
