404480624dad8103f4be2ddf56ea8b8644960f51
ivan-suarez <heyitsivan@protonmail.com>
2021-07-06 14:28:30 +0200

    Started header structure

--- src/Header.js
+++ src/Header.js
@@ -3,9 +3,28 @@
 
  export default function Header() {
      return (
-         <div className="header">
-             <p>Header</p>
-         </div>
+        <div className="header">
+             <img src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"/>
+        
+        <div className="header__search">
+            <input className="header__searchInput" type="text"/>
+        </div>
+        <div className="header__nav">
+        <div className="header__option">
+            <span className="header_optionLineOne">Hello Guest</span>
+            <span className="header_optionLineTwo">Sign In</span>
+
+        </div>
+        <div className="header__option">
+        <span className="header_optionLineOne">Returns</span>
+            <span className="header_optionLineTwo">Orders</span>
+        </div>
+        <div className="header__option">
+        <span className="header_optionLineOne">Your</span>
+            <span className="header_optionLineTwo">Prime</span>
+        </div>
+        </div>
+        </div>
      )
  }
  
