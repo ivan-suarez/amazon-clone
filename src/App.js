a2e14f0dc4dc140372843d826e499f9a9159200f
ivan-suarez <heyitsivan@protonmail.com>
2021-07-10 10:30:38 +0200

    Created React Router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import './App.css';
 import Header from './Header';
 import Home from './Home'; 
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom"
 
 function App() {
   return (
+    <Router>
     <div className="app">
-      
-      {/** Header */}
+      <Switch>
+        <Route path="/checkout">
+        {/** Header */}
+      <Header/>
+      {/** Home */}
+      <p>I'm a checkout</p>
+      </Route>
+
+      <Route path="/">
+        {/** Header */}
       <Header/>
       {/** Home */}
       <Home/>
+      </Route>
+      </Switch>
+      
     </div>
+    </Router>
 
   );
 }
