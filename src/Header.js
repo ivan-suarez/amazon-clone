13816b90e80f24f55568a0ba033e26764091ca10
ivan-suarez <heyitsivan@protonmail.com>
2021-07-10 10:56:14 +0200

    Added router links

--- src/Header.js
+++ src/Header.js
@@ -2,12 +2,15 @@
  import "./Header.css"
  import SearchIcon from "@material-ui/icons/Search";
  import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import {Link} from "react-router-dom"
 
  export default function Header() {
      return (
         <div className="header">
+            <Link to="/">
+            
              <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"/>
-        
+             </Link>
         <div className="header__search">
             <input className="header__searchInput" type="text"/>
             <SearchIcon className="header__searchIcon"/>
@@ -26,11 +29,12 @@
         <span className="header__ optionLineOne">Your</span>
             <span className="header__optionLineTwo">Prime</span>
         </div>
-
+        <Link to="/checkout">
         <div className="header__optionBasket">
             <ShoppingBasketIcon/>
             <span className="header__optionLineTwo header__basketCount">0</span>
         </div>
+        </Link>
 
         </div>
         </div>
