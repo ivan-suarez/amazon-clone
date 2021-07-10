f81f61335c08934d0ccca6f6cd8c08ec43b3f56e
ivan-suarez <heyitsivan@protonmail.com>
2021-07-10 10:51:40 +0200

    Created checkout page

--- src/App.js
+++ src/App.js
@@ -2,22 +2,25 @@ import './App.css';
 import Header from './Header';
 import Home from './Home'; 
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom"
+import Checkout from './Checkout'
 
 function App() {
   return (
     <Router>
     <div className="app">
+    <Header/>
       <Switch>
         <Route path="/checkout">
         {/** Header */}
-      <Header/>
+      
       {/** Home */}
-      <p>I'm a checkout</p>
+      
+      <Checkout/>
       </Route>
 
       <Route path="/">
         {/** Header */}
-      <Header/>
+      
       {/** Home */}
       <Home/>
       </Route>
