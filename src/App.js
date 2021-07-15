655152ed4cb7b5ebaadcdd9a203d04ca11db0c8b
ivan-suarez <heyitsivan@protonmail.com>
2021-07-15 11:17:55 +0200

    Added payment page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Login from './Login'
 import React, {useEffect} from 'react'
 import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
+import Payment from './Payment'
 
 function App() {
   const [{}, dispatch] = useStateValue();
@@ -49,7 +50,10 @@ function App() {
         <Header/>
       <Checkout/>
       </Route>
-
+      <Route path="/payment">
+        <Header/>
+      <Payment/>
+      </Route>
      
 
       <Route path="/">
